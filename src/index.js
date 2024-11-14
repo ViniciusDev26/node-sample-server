0fddac312a1e4d69f2d28bcbdf6fc71d12bc2f13
Carlos Vinicius <viniciusdev.26@gmail.com>
2024-11-14 00:13:16 -0300

    create login route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const { z } = require("zod");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 const app = express();
@@ -83,6 +84,34 @@ app.put("/customer/:id", async (req, res) => {
 	});
 
 	res.json(customer);
+});
+
+app.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+
+	const customer = await prisma.customer.findUnique({
+		where: { email },
+	});
+	if (!customer) return res.status(404).json({ error: "Customer not found" });
+
+	const password_match = await bcrypt.compare(password, customer.password);
+	if (!password_match)
+		return res.status(404).json({ error: "Customer not found" });
+
+	const token = jwt.sign(
+		{
+			email: customer.email,
+		},
+		"aaaa",
+		{
+			expiresIn: "1h",
+			subject: String(customer.id),
+		},
+	);
+
+	res.json({
+		token,
+	});
 });
 
 app.listen(3000, () => {
